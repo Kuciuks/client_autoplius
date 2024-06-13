fa0fd93338237ae9292ba05b526a96c325f7104e
Benas <benas.sarachovas@gmail.com>
2024-06-13 21:31:16 +0300

    saving header(column) names to csv

--- write_to_csv.py
+++ write_to_csv.py
@@ -3,11 +3,15 @@ import data_retriever
 
 
 def write_to_csv(data):
-    
+    #go through each individual element inside the data array
     for element in data:
-        print(element)
+        #go through details of the element
+        # for details in element:
+            # print(details.text)
+        # print(element)
+        #capture element names as column values
         header = [item.tag for item in element]
-        print(header)
+        # print(header)
         
         with open('products_data.csv','w',newline='') as csvfile:
             writer = csv.writer(csvfile)
