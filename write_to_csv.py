753889178777d15853601d5016eb28700d567cac
Benas <benas.sarachovas@gmail.com>
2024-06-20 19:09:07 +0300

    implementing individual product detail parsing and saving to csv files (not done)

--- write_to_csv.py
+++ write_to_csv.py
@@ -3,21 +3,19 @@ import data_retriever,config
 
 
 def write_to_csv():
-    print(config.product_type_storage)
+    products = config.product_type_storage
     
     #go through each individual element inside the data array
-    # for element in data:
-    #     #go through details of the element
-    #     # for details in element:
-    #         # print(details.text)
-    #     # print(element)
-    #     #capture element names as column values
-    #     header = [item.tag for item in element]
-    #     # print(header)
+    for element in products:
+        #go through details of the element
+        for details in element:
+            print(details, details.tag, element)
+        # #capture element names as column values
+        # header = [item.tag for item in details]
         
-    #     with open('products_data.csv','w',newline='') as csvfile:
-    #         writer = csv.writer(csvfile)
-    #         writer.writerow(header)
-    #         csvfile.close()
+        # with open(f'{element.tag}.csv','w',newline='') as csvfile:
+        #     writer = csv.writer(csvfile)
+        #     writer.writerow(header)
+        #     csvfile.close()
             
             
