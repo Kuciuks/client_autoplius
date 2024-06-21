9c4050029278a51f58064e48534e003750592ac1
Benas <benas.sarachovas@gmail.com>
2024-06-21 15:07:19 +0300

    writing product values to csv

--- write_to_csv.py
+++ write_to_csv.py
@@ -6,13 +6,17 @@ def product_to_csv():
     products = config.product_type_storage
     
     #go through each individual element inside the data array
-    for element in products:
-        # #capture element names as column values
-        collumn_names = [details.tag for details in element[0]]
-        
-        with open(f'{element.tag}.csv','w',newline='') as csvfile:
+    for product in products:
+        #capture product column values for products
+        collumn_names = [details.tag for details in product[0]]
+        with open(f'{product.tag}.csv','w',newline='',encoding='utf-8') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(collumn_names)
+            
+            #get details for each new product
+            for element in product:
+                values = [values.text for values in element]
+                writer.writerow(values)            
             csvfile.close()
             
             
